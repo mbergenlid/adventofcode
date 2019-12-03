a28145b0ecfc62ea38e74bbd367dce9e5a600145
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2019-12-03 07:42:37 +0100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod prob1;
+mod prob2;
 
 fn main() {
-    prob1::solve_part_1();
-    prob1::solve_part_2();
+    //prob1::solve_part_1();
+    //prob1::solve_part_2();
+    prob2::solve_part_1();
+    prob2::solve_part_2();
 }
