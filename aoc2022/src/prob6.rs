838960e78d234cbd7810b7f68e48b53350813da7
Marcus Bergenlid <mbergenlid@invidi.com>
2022-12-06 08:42:43 +0100

    Day 6

--- aoc2022/src/prob6.rs
+++ aoc2022/src/prob6.rs
@@ -1,7 +1,25 @@
-pub fn solve_part_1(_input: &str) -> usize {
-    todo!()
+use itertools::Itertools;
+
+pub fn solve_part_1(input: &str) -> usize {
+    for index in 0..input.len()-4 {
+        let range = &input[index..index+4];
+        if is_unique(range) {
+            return index+4;
+        }
+    }
+    panic!("Not found")
 }
 
-pub fn solve_part_2(_input: &str) -> usize {
-    todo!()
+fn is_unique(input: &str) -> bool {
+    !input.is_empty() && input.chars().all_unique()
+}
+
+pub fn solve_part_2(input: &str) -> usize {
+    for index in 0..input.len()-14 {
+        let range = &input[index..index+14];
+        if is_unique(range) {
+            return index+14;
+        }
+    }
+    panic!("Not found")
 }
