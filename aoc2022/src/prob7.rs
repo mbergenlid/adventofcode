f6a14162c2e6bb2cd35332dbe4c992bf1524c3b9
Marcus Bergenlid <mbergenlid@invidi.com>
2022-12-08 10:22:05 +0100

    Day 7 and 8

--- aoc2022/src/prob7.rs
+++ aoc2022/src/prob7.rs
@@ -1,7 +1,128 @@
-pub fn solve_part_1(_input: &str) -> usize {
-    todo!()
+use std::cmp::min;
+use crate::prob7::Node::{Directory, File};
+use itertools::Itertools;
+use std::fmt::format;
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Node {
+    Directory(String, Vec<Node>),
+    File(String, usize),
 }
 
-pub fn solve_part_2(_input: &str) -> usize {
-    todo!()
+impl Node {
+    fn name(&self) -> &str {
+        match self {
+            Node::Directory(n, _) => n.as_str(),
+            Node::File(n, _) => n.as_str(),
+        }
+    }
+}
+
+impl FromStr for Node {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.starts_with("dir") {
+            Ok(Directory(s["dir ".len()..].to_string(), Vec::new()))
+        } else {
+            let mut size_and_name = s.split(" ");
+            let size = size_and_name
+                .next()
+                .unwrap()
+                .parse()
+                .expect(&format!("Unable to parse size: {}", s));
+            Ok(File(size_and_name.next().expect("").to_string(), size))
+        }
+    }
+}
+
+fn read_directory_tree<'a, I>(lines: &mut I, current_dir: &mut Node)
+where
+    I: Iterator<Item = &'a str> + Clone,
+{
+    if let Node::Directory(name, children) = current_dir {
+        while let Some(line) = lines.next() {
+            if line.starts_with("$ cd ..") {
+                return;
+            } else if line.starts_with("$ cd") {
+                let to_dir = &line["$ cd ".len()..];
+                let dir = children
+                    .iter_mut()
+                    .find(|c| c.name() == to_dir)
+                    .expect(&format!("Couldn't find directory {}", to_dir));
+                read_directory_tree(lines, dir);
+            } else if line.starts_with("$ ls") {
+                let mut c: Vec<_> = lines
+                    .clone()
+                    .take_while(|l| !l.starts_with("$"))
+                    .map(|line| line.parse::<Node>().unwrap())
+                    .collect();
+
+                children.append(&mut c);
+            }
+        }
+    } else {
+        panic!("Not a directory");
+    }
+}
+
+fn size(node: &Node) -> usize {
+    match node {
+        Directory(_, children) => children.iter().map(|c| size(c)).sum(),
+        File(_, size) => *size,
+    }
+}
+
+fn find_nodes_with_size(node: &Node) -> usize {
+    match node {
+        Directory(_, children) => {
+            let size = size(node);
+            let children_size = children.iter().map(|c| find_nodes_with_size(c)).sum::<usize>();
+            if size <= 100000 {
+                size + children_size
+            } else {
+                children_size
+            }
+        }
+        File(_, _) => 0
+    }
+}
+
+pub fn solve_part_1(input: &str) -> usize {
+    let mut root = Directory("/".to_string(), Vec::new());
+    read_directory_tree(&mut input.lines().skip(1), &mut root);
+
+    //println!("{:?}", root);
+    find_nodes_with_size(&root)
+
+}
+
+pub fn solve_part_2(input: &str) -> usize {
+    let mut root = Directory("/".to_string(), Vec::new());
+    read_directory_tree(&mut input.lines().skip(1), &mut root);
+
+
+    let used_space = size(&root);
+    let additional_free_space_required = 30000000 - (70000000 - used_space);
+    find_smallest_dir_with_size(&root, additional_free_space_required, usize::MAX)
+}
+
+
+fn find_smallest_dir_with_size(node: &Node, at_least: usize, mut smallest_dir_so_far: usize) -> usize {
+    match node {
+        Directory(_, children) => {
+            let size = size(node);
+            if size >= at_least {
+                smallest_dir_so_far = min(smallest_dir_so_far, size);
+                let children_smallest = children.iter()
+                    .map(|c| find_smallest_dir_with_size(c, at_least, smallest_dir_so_far)).min().unwrap();
+
+                min(smallest_dir_so_far, children_smallest)
+            } else {
+                usize::MAX
+            }
+        }
+        File(_, _) => usize::MAX
+    }
 }
--- aoc2022/src/prob8.rs
+++ aoc2022/src/prob8.rs
@@ -1,7 +1,192 @@
-pub fn solve_part_1(_input: &str) -> usize {
-    todo!()
+use std::cmp::max;
+use std::env::current_dir;
+use std::fmt::format;
+
+type Grid = Vec<Vec<u8>>;
+
+fn read_tree_grid(input: &str) -> Vec<Vec<u8>> {
+    let mut result = Vec::new();
+    for line in input.lines() {
+        let mut row = Vec::new();
+
+        for digit in line.chars() {
+            if digit == '\n' {
+                continue;
+            }
+            row.push(
+                digit
+                    .to_digit(10)
+                    .expect(&format!("Unable to parse digit {}", digit)) as u8,
+            );
+        }
+
+        result.push(row)
+    }
+
+    result
 }
 
-pub fn solve_part_2(_input: &str) -> usize {
-    todo!()
+pub fn solve_part_1(input: &str) -> usize {
+    let grid = read_tree_grid(input);
+
+    let mut count = 0;
+    for row in 1..(grid.len() - 1) {
+        for col in 1..(grid[0].len() - 1) {
+            if tree_viewable(&grid, (row, col), |(r, c)| {
+                if r == 0 {
+                    None
+                } else {
+                    Some((r - 1, c))
+                }
+            }) {
+                count += 1;
+            } else if tree_viewable(&grid, (row, col), |(r, c)| {
+                if r == grid.len() {
+                    None
+                } else {
+                    Some((r + 1, c))
+                }
+            }) {
+                count += 1;
+            } else if tree_viewable(&grid, (row, col), |(r, c)| {
+                if c == 0 {
+                    None
+                } else {
+                    Some((r, c - 1))
+                }
+            }) {
+                count += 1;
+            } else if tree_viewable(&grid, (row, col), |(r, c)| {
+                if c == grid.len() {
+                    None
+                } else {
+                    Some((r, c + 1))
+                }
+            }) {
+                count += 1;
+            }
+        }
+    }
+
+    count + grid.len()*2 + (grid[0].len()-2)*2
+}
+
+fn tree_viewable<F>(grid: &Grid, pos: (usize, usize), next: F) -> bool
+where
+    F: Fn((usize, usize)) -> Option<(usize, usize)>,
+{
+    let mut prev_pos = pos;
+    let mut original_height = grid[pos.0][pos.1];
+    while let Some(next_pos) = next(prev_pos) {
+        let next_height = grid.get(next_pos.0).and_then(|row| row.get(next_pos.1));
+
+        match next_height {
+            None => return true,
+            Some(&height) => {
+                if height >= original_height {
+                    return false;
+                }
+            }
+        }
+
+        prev_pos = next_pos;
+    }
+    true
+}
+
+pub fn solve_part_2(input: &str) -> usize {
+    let grid = read_tree_grid(input);
+
+    let mut max_scenic_score = 0;
+    for row in 1..(grid.len() - 1) {
+        for col in 1..(grid[0].len() - 1) {
+            let mut scenic_score = 1;
+            scenic_score *= viewing_distance(&grid, (row, col), |(r, c)| {
+                if r == 0 {
+                    None
+                } else {
+                    Some((r - 1, c))
+                }
+            });
+            scenic_score *= viewing_distance(&grid, (row, col), |(r, c)| {
+                if r == grid.len() {
+                    None
+                } else {
+                    Some((r + 1, c))
+                }
+            });
+            scenic_score *= viewing_distance(&grid, (row, col), |(r, c)| {
+                if c == 0 {
+                    None
+                } else {
+                    Some((r, c - 1))
+                }
+            });
+            scenic_score *= viewing_distance(&grid, (row, col), |(r, c)| {
+                if c == grid.len() {
+                    None
+                } else {
+                    Some((r, c + 1))
+                }
+            });
+
+            max_scenic_score = max(max_scenic_score, scenic_score);
+        }
+    }
+
+    max_scenic_score
+
+}
+
+fn viewing_distance<F>(grid: &Grid, pos: (usize, usize), next: F) -> usize
+    where
+        F: Fn((usize, usize)) -> Option<(usize, usize)>,
+{
+    let mut count = 0;
+    let mut prev_pos = pos;
+    let mut original_height = grid[pos.0][pos.1];
+    while let Some(next_pos) = next(prev_pos) {
+        let next_height = grid.get(next_pos.0).and_then(|row| row.get(next_pos.1));
+
+        match next_height {
+            None => return count,
+            Some(&height) => {
+                count += 1;
+                if height >= original_height {
+                    return count;
+                }
+            }
+        }
+
+        prev_pos = next_pos;
+    }
+    count
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::prob8::{solve_part_1, solve_part_2};
+
+    #[test]
+    fn test_part1() {
+        let input = r"30373
+25512
+65332
+33549
+35390";
+
+        println!("{}", solve_part_1(input));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = r"30373
+25512
+65332
+33549
+35390";
+
+        println!("{}", solve_part_2(input));
+    }
 }
