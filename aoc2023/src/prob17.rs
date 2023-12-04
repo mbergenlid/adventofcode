c37075b78798e34bfe4dd7362682bdfb66ababb9
Marcus Bergenlid <mbergenlid@invidi.com>
2023-12-04 09:36:31 +0100

    Add default tests to all days

--- aoc2023/src/prob17.rs
+++ aoc2023/src/prob17.rs
@@ -5,3 +5,19 @@ pub fn solve_part_1(_input: &str) -> usize {
 pub fn solve_part_2(_input: &str) -> usize {
     todo!()
 }
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn solve_part_1() {
+        assert_eq!(super::solve_part_1(TEST_INPUT), 0);
+    }
+
+    #[test]
+    fn solve_part_2() {
+        assert_eq!(super::solve_part_2(TEST_INPUT), 0);
+    }
+
+    const TEST_INPUT: &'static str = "";
+}
