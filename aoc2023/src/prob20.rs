46600427a778d035f9dfd8bfcb0dc3c768362f77
Marcus Bergenlid <mbergenlid@invidi.com>
2023-12-27 22:22:41 +0100

    Day21

--- aoc2023/src/prob20.rs
+++ aoc2023/src/prob20.rs
@@ -1,23 +1,313 @@
-pub fn solve_part_1(_input: &str) -> usize {
-    todo!()
+use std::{
+    collections::{HashMap, VecDeque},
+    str::FromStr,
+};
+
+use itertools::Itertools;
+
+pub fn solve_part_1(input: &str) -> usize {
+    let mut circuit = input.parse::<Circuit>().unwrap();
+
+    let mut low_count = 0;
+    let mut high_count = 0;
+    for _ in 0..1000 {
+        let (low, high) = circuit.run(0);
+        low_count += low;
+        high_count += high;
+    }
+    dbg!(low_count) * dbg!(high_count)
 }
 
-pub fn solve_part_2(_input: &str) -> usize {
-    todo!()
+pub fn solve_part_2(input: &str) -> usize {
+    let mut circuit = input.parse::<Circuit>().unwrap();
+
+    // !output = !con
+    // !con =  b & a
+    // !inv = !a => i mod 2 = 1
+    // b    =
+    // a    = i mod 2 = 0
+
+    let mut msg_count = 0;
+    for i in 0..1000 {
+        let (low, high) = circuit.run(msg_count);
+        msg_count += low + high;
+    }
+
+    if let ModuleType::FlipFlop(_, state_changes) =
+        &circuit.modules.iter().find(|m| m.name == "kr").unwrap().tpe
+    {
+        for diff in state_changes
+            .iter()
+            .zip(state_changes.iter().skip(1))
+            .map(|(x1, x2)| x2.1 - x1.1)
+        {
+            println!("{}", diff);
+        }
+    }
+    unreachable!()
 }
 
+#[derive(Debug)]
+struct Circuit {
+    modules: Vec<Module>,
+    broadcaster: usize,
+    module_names: HashMap<String, usize>,
+}
+
+impl Circuit {
+    fn run(&mut self, msg_count: usize) -> (usize, usize) {
+        let broadcaster_index = self.broadcaster;
+        let mut queue = VecDeque::new();
+        queue.push_back(Message {
+            from: usize::MAX,
+            to: broadcaster_index,
+            value: false,
+            counter: msg_count,
+        });
+        let mut low_count = 0;
+        let mut high_count = 0;
+        while let Some(msg) = queue.pop_front() {
+            let module = &mut self.modules[msg.to];
+            for out in module.send(msg) {
+                if out.value {
+                    high_count += 1;
+                } else {
+                    low_count += 1;
+                }
+                if out.to < usize::MAX - 1 {
+                    queue.push_back(out);
+                }
+            }
+        }
+
+        (low_count + 1, high_count)
+    }
+
+    fn run2(&mut self, index: i32) -> bool {
+        let mut output_seen_false = false;
+        let flip_flop_a = *self.module_names.get("a").unwrap();
+        let flip_flop_b = *self.module_names.get("b").unwrap();
+        let broadcaster_index = self.broadcaster;
+        let mut queue = VecDeque::new();
+        queue.push_back(Message {
+            from: usize::MAX,
+            to: broadcaster_index,
+            value: false,
+            counter: 0,
+        });
+        while let Some(msg) = queue.pop_front() {
+            let module = &mut self.modules[msg.to];
+            let to_send = module.send(msg);
+            if let Some(msg) = to_send.iter().next() {
+                if msg.from == flip_flop_a {
+                    println!("{} a => {}", index, msg.value);
+                }
+            }
+            if let Some(msg) = to_send.iter().next() {
+                if msg.from == flip_flop_b {
+                    println!("{} b => {}", index, msg.value);
+                }
+            }
+            for out in to_send {
+                if out.to < usize::MAX - 1 {
+                    queue.push_back(out);
+                } else if out.to == usize::MAX - 1 {
+                    if !out.value {
+                        output_seen_false = true;
+                    }
+                }
+            }
+        }
+
+        return output_seen_false;
+    }
+}
+
+struct Message {
+    from: usize,
+    to: usize,
+    value: bool,
+    counter: usize,
+}
+
+#[derive(Clone, Debug)]
+struct Module {
+    outputs: Vec<usize>,
+    tpe: ModuleType,
+    name: String,
+}
+
+impl Module {
+    fn send(&mut self, msg: Message) -> Vec<Message> {
+        let output = match &mut self.tpe {
+            ModuleType::Broadcaster => msg.value,
+            ModuleType::FlipFlop(state, state_changes) => {
+                if msg.value {
+                    return Vec::new();
+                } else {
+                    *state = !*state;
+                    state_changes.push((*state, msg.counter));
+                    *state
+                }
+            }
+            ModuleType::Conjunction(inputs) => {
+                inputs[msg.from] = msg.value;
+                !inputs.into_iter().all(|input| *input)
+            }
+        };
+        self.forward(msg.to, output, msg.counter)
+    }
+
+    fn forward(&self, from: usize, value: bool, start_counter: usize) -> Vec<Message> {
+        self.outputs
+            .iter()
+            .enumerate()
+            .map(|(i, o)| Message {
+                from,
+                to: *o,
+                value,
+                counter: start_counter + 1 + i,
+            })
+            .collect()
+    }
+}
+
+#[derive(Clone, Debug)]
+enum ModuleType {
+    Broadcaster,
+    FlipFlop(bool, Vec<(bool, usize)>),
+    Conjunction(Vec<bool>),
+}
+
+impl FromStr for Circuit {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut name_map = HashMap::<String, usize>::new();
+        let mut broadcaster = 0;
+        for (index, line) in s.lines().enumerate() {
+            let name = line.split(" -> ").next().unwrap();
+            if name == "broadcaster" {
+                broadcaster = index;
+                name_map.insert(name.to_string(), index);
+            } else {
+                name_map.insert(name[1..].to_string(), index);
+            }
+        }
+
+        let name_map = dbg!(name_map);
+        let mut modules = dbg!(vec![None; name_map.len()]);
+        let mut input_map = vec![Vec::new(); name_map.len()];
+        for line in s.lines() {
+            let (name_and_type, outputs) = line.split(" -> ").collect_tuple().unwrap();
+            let outputs = outputs
+                .split(", ")
+                .map(|o| {
+                    *name_map.get(o).unwrap_or_else(|| {
+                        if o == "rx" {
+                            &(usize::MAX - 1)
+                        } else {
+                            &usize::MAX
+                        }
+                    })
+                })
+                .collect::<Vec<_>>();
+
+            let my_index = index_of(&name_map, name_and_type);
+            for output in outputs.iter().filter(|&o| *o < usize::MAX - 1) {
+                input_map[*output].push(my_index);
+            }
+            if name_and_type == "broadcaster" {
+                modules[my_index] = Some(Module {
+                    tpe: ModuleType::Broadcaster,
+                    outputs: outputs,
+                    name: "broadcaster".to_string(),
+                });
+            } else if name_and_type.starts_with("%") {
+                modules[my_index] = Some(Module {
+                    outputs: outputs,
+                    tpe: ModuleType::FlipFlop(false, Vec::new()),
+                    name: name_and_type[1..].to_string(),
+                });
+            } else if name_and_type.starts_with("&") {
+                modules[my_index] = Some(Module {
+                    outputs: outputs,
+                    tpe: ModuleType::Conjunction(vec![true; name_map.len()]),
+                    name: name_and_type[1..].to_string(),
+                });
+            }
+        }
+
+        fn index_of(name_map: &HashMap<String, usize>, name: &str) -> usize {
+            if name == "broadcaster" {
+                *name_map.get(name).unwrap()
+            } else if name.starts_with("%") {
+                *name_map.get(&name[1..]).unwrap()
+            } else if name.starts_with("&") {
+                *name_map.get(&name[1..]).unwrap()
+            } else {
+                unreachable!()
+            }
+        }
+
+        let modules = dbg!(modules);
+        let mut result = Vec::new();
+
+        for (index, module) in modules.into_iter().enumerate() {
+            assert!(index == name_map.len() || module.is_some());
+            if let Some(mut module) = module {
+                if let ModuleType::Conjunction(x) = &mut module.tpe {
+                    for out in &input_map[index] {
+                        x[*out] = false;
+                    }
+                }
+                result.push(module);
+            }
+        }
+
+        Ok(Circuit {
+            modules: result,
+            broadcaster,
+            module_names: name_map,
+        })
+    }
+}
+
+// !rx = !bn
+// !bn = lcm(!pl, !mz, !lz, !zm)
+// !pl = !qt
+// !
+
+// !rx = bn
+// !bn = pl & mz & lz & zm
+//
+// pl
+// kr
+
+//
 #[cfg(test)]
 mod test {
 
     #[test]
     fn solve_part_1() {
-        assert_eq!(super::solve_part_1(TEST_INPUT), 0);
+        assert_eq!(super::solve_part_1(TEST_INPUT), 32000000);
+        assert_eq!(super::solve_part_1(TEST_INPUT_2), 11687500);
     }
 
     #[test]
     fn solve_part_2() {
-        assert_eq!(super::solve_part_2(TEST_INPUT), 0);
+        assert_eq!(super::solve_part_2(TEST_INPUT_2), 0);
     }
 
-    const TEST_INPUT: &'static str = "";
+    const TEST_INPUT: &'static str = "broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a";
+
+    const TEST_INPUT_2: &'static str = "broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output";
 }
--- aoc2023/src/prob21.rs
+++ aoc2023/src/prob21.rs
@@ -1,23 +1,427 @@
-pub fn solve_part_1(_input: &str) -> usize {
-    todo!()
+use std::{collections::VecDeque, fmt::Debug, str::FromStr};
+
+pub fn solve_part_1(input: &str) -> usize {
+    let grid = input.parse::<Grid<64>>().unwrap();
+    solve_1(grid)
 }
 
-pub fn solve_part_2(_input: &str) -> usize {
-    todo!()
+fn solve_1<const N: usize>(mut grid: Grid<N>) -> usize {
+    let (row, col) = grid.start;
+
+    grid.solve(row, col, 0);
+    grid.evens()
+}
+
+pub fn solve_part_2(input: &str) -> usize {
+    solve(input.parse::<Grid<26501365>>().unwrap())
+}
+
+fn solve<const N: usize>(template: Grid<N>) -> usize {
+    let (start_row, start_col) = template.start;
+    let mut start_grid = template.clone();
+    start_grid.solve(start_row, start_col, 0);
+
+    let mut total = start_grid.odds_starting_at(0);
+    {
+        {
+            let mut right = template.clone();
+            right.solve(start_row, 0, 0);
+            let max = right.max();
+            let even_count = right.odds_starting_at(0);
+            let odd_count = right.odds_starting_at(1);
+            let something = (template.width - 1 - start_col) + max;
+            let cols = if something > N {
+                0
+            } else {
+                (N - something) / template.width
+            };
+            total += (cols / 2 + cols % 2) * even_count * 4;
+            total += (cols / 2) * odd_count * 4;
+
+            for col in cols.. {
+                let this_tile =
+                    right.odds_starting_at(col * template.width + (template.width - start_col));
+                total += this_tile;
+
+                if this_tile == 0 {
+                    break;
+                }
+            }
+            let mut left = template.clone();
+            left.solve(start_row, template.width - 1, 0);
+            for col in cols.. {
+                let this_tile = left.odds_starting_at(col * template.width + start_col + 1);
+                total += this_tile;
+
+                if this_tile == 0 {
+                    break;
+                }
+            }
+            let mut up = template.clone();
+            up.solve(template.height - 1, start_col, 0);
+            for row in cols.. {
+                let this_tile = up.odds_starting_at(row * template.height + start_row + 1);
+                total += this_tile;
+
+                if this_tile == 0 {
+                    break;
+                }
+            }
+            let mut down = template.clone();
+            down.solve(0, start_col, 0);
+            for row in cols.. {
+                let this_tile =
+                    down.odds_starting_at(row * template.height + (template.height - start_row));
+                total += this_tile;
+
+                if this_tile == 0 {
+                    break;
+                }
+            }
+        }
+    }
+
+    // Down
+    {
+        let mut right = template.clone();
+        right.solve(0, 0, 0);
+        let max_right = right.max();
+        let mut left = template.clone();
+        left.solve(0, template.width - 1, 0);
+        let even_count_left = left.odds_starting_at(0);
+        let odd_count_left = left.odds_starting_at(1);
+        let max_left = left.max();
+        {
+            let something = 0 * template.height
+                + (template.height - 1 - start_row)
+                + 0 * template.width
+                + (template.width - 1 - start_col)
+                + max_right;
+            let cols = if something > N {
+                0
+            } else {
+                (N - something) / template.width
+            };
+            if cols % 2 == 0 {
+                let number_of_odd_lines = cols;
+                let total_filled = cols * (cols + 1);
+                total += ((total_filled - number_of_odd_lines) / 2) * even_count_left;
+                total += ((total_filled - number_of_odd_lines) / 2) * odd_count_left;
+                total += number_of_odd_lines * odd_count_left
+            } else {
+                let number_of_odd_lines = (cols / 2 + 1) * 2;
+                let total_filled = (cols) * (cols + 1);
+                total += ((total_filled - number_of_odd_lines) / 2) * even_count_left;
+                total += ((total_filled - number_of_odd_lines) / 2) * odd_count_left;
+                total += number_of_odd_lines * even_count_left
+            }
+        }
+        {
+            let something = 0 * template.height
+                + (template.height - 1 - start_row)
+                + 0 * template.width
+                + (template.width - 1 - start_col)
+                + max_right;
+            let col = if something > N {
+                0
+            } else {
+                (N - something) / template.width
+            };
+            for col in col.. {
+                let this_tile = right.odds_starting_at(
+                    0 * template.height
+                        + (template.height - 1 - start_row)
+                        + col * template.width
+                        + (template.width - 1 - start_col)
+                        + 2,
+                );
+                total += this_tile * (col + 1);
+
+                if this_tile == 0 {
+                    break;
+                }
+            }
+        }
+        {
+            let something = 0 * template.height
+                + (template.height - 1 - start_row)
+                + 0 * template.width
+                + (template.width - 1 - start_col)
+                + max_left;
+            let col = if something > N {
+                0
+            } else {
+                (N - something) / template.width
+            };
+            for col in col.. {
+                let this_tile = left.odds_starting_at(
+                    0 * template.height
+                        + (template.height - 1 - start_row)
+                        + col * template.width
+                        + start_col
+                        + 2,
+                );
+                total += this_tile * (col + 1);
+
+                if this_tile == 0 {
+                    break;
+                }
+            }
+        }
+    }
+    // Up
+    {
+        let mut right = template.clone();
+        right.solve(template.height - 1, 0, 0);
+        let max_right = right.max();
+        let mut left = template.clone();
+        left.solve(template.height - 1, template.width - 1, 0);
+        let even_count_left = left.odds_starting_at(0);
+        let odd_count_left = left.odds_starting_at(1);
+        let something = 0 * template.height
+            + start_row
+            + 0 * template.width
+            + (template.width - 1 - start_col)
+            + max_right;
+        let cols = if something > N {
+            0
+        } else {
+            (N - something) / template.width
+        };
+        if cols % 2 == 0 {
+            let number_of_odd_lines = cols;
+            let total_filled = cols * (cols + 1);
+            total += ((total_filled - number_of_odd_lines) / 2) * even_count_left;
+            total += ((total_filled - number_of_odd_lines) / 2) * odd_count_left;
+            total += number_of_odd_lines * odd_count_left
+        } else {
+            let number_of_odd_lines = (cols / 2 + 1) * 2;
+            let total_filled = (cols) * (cols + 1);
+            total += ((total_filled - number_of_odd_lines) / 2) * even_count_left;
+            total += ((total_filled - number_of_odd_lines) / 2) * odd_count_left;
+            total += number_of_odd_lines * even_count_left
+        }
+        for col in cols.. {
+            let this_tile = right.odds_starting_at(
+                0 * template.height
+                    + start_row
+                    + col * template.width
+                    + (template.width - 1 - start_col)
+                    + 2,
+            );
+            total += this_tile * (col + 1);
+
+            if this_tile == 0 {
+                break;
+            }
+        }
+        for col in cols.. {
+            let this_tile = left.odds_starting_at(
+                0 * template.height + start_row + col * template.width + start_col + 2,
+            );
+            total += this_tile * (col + 1);
+
+            if this_tile == 0 {
+                break;
+            }
+        }
+    }
+
+    total
+}
+
+#[derive(PartialEq, Eq, Clone)]
+enum Tile {
+    Wall,
+    Step(usize),
+}
+
+impl Tile {
+    fn steps(&self) -> Option<usize> {
+        match self {
+            Tile::Wall => None,
+            Tile::Step(s) => Some(*s),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Grid<const N: usize> {
+    data: Vec<Vec<Tile>>,
+    width: usize,
+    height: usize,
+    start: (usize, usize),
+}
+
+impl<const N: usize> Debug for Grid<N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for row in &self.data {
+            for col in row {
+                match col {
+                    Tile::Wall => write!(f, "#")?,
+                    Tile::Step(s) => {
+                        if *s <= N && s % 2 == 1 {
+                            write!(f, "+")?
+                        } else if *s == usize::MAX {
+                            write!(f, "*")?
+                        } else {
+                            write!(f, ".")?
+                        }
+                    }
+                }
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
+
+impl<const N: usize> Grid<N> {
+    fn solve(&mut self, row: usize, col: usize, step_count: usize) {
+        let mut queue = VecDeque::new();
+        queue.push_back((step_count, row, col));
+        while let Some((steps, row, col)) = queue.pop_front() {
+            if self.data[row][col] == Tile::Wall {
+                continue;
+            }
+            if let Tile::Step(c) = &mut self.data[row][col] {
+                if *c <= steps {
+                    continue;
+                } else {
+                    *c = steps;
+                }
+            }
+            //Up
+            {
+                if row > 0 {
+                    queue.push_back((steps + 1, row - 1, col));
+                }
+            }
+            //Right
+            {
+                if col < self.width - 1 {
+                    queue.push_back((steps + 1, row, col + 1));
+                }
+            }
+            //Down
+            {
+                if row < self.height - 1 {
+                    queue.push_back((steps + 1, row + 1, col));
+                }
+            }
+            //Left
+            {
+                if col > 0 {
+                    queue.push_back((steps + 1, row, col - 1));
+                }
+            }
+        }
+    }
+
+    fn evens(&self) -> usize {
+        self.data
+            .iter()
+            .flat_map(|r| r.iter().filter_map(|c| c.steps()))
+            .filter(|s| *s != usize::MAX && *s <= N)
+            .filter(|s| *s % 2 == 0)
+            .count()
+    }
+
+    fn odds_starting_at(&self, start: usize) -> usize {
+        self.data
+            .iter()
+            .flat_map(|r| r.iter().filter_map(|c| c.steps()))
+            .filter(|s| *s != usize::MAX && *s + start <= N)
+            .filter(|s| (*s + start) % 2 == 1)
+            .count()
+    }
+
+    fn max(&self) -> usize {
+        self.data
+            .iter()
+            .flat_map(|r| r.iter().filter_map(|c| c.steps()))
+            .filter(|s| *s != usize::MAX)
+            .max()
+            .unwrap()
+    }
+}
+
+impl<const N: usize> FromStr for Grid<N> {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut start = None;
+        let data: Vec<Vec<_>> = s
+            .lines()
+            .enumerate()
+            .map(|(row, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(col, c)| match c {
+                        'S' => {
+                            start = Some((row, col));
+                            Tile::Step(usize::MAX)
+                        }
+                        '.' => Tile::Step(usize::MAX),
+                        '#' => Tile::Wall,
+                        _ => unreachable!(),
+                    })
+                    .collect()
+            })
+            .collect();
+        let width = data[0].len();
+        let height = data.len();
+
+        Ok(Grid {
+            data,
+            width,
+            height,
+            start: start.unwrap(),
+        })
+    }
 }
 
 #[cfg(test)]
 mod test {
+    use crate::prob21::Grid;
 
     #[test]
     fn solve_part_1() {
-        assert_eq!(super::solve_part_1(TEST_INPUT), 0);
+        assert_eq!(super::solve_1(TEST_INPUT.parse::<Grid<6>>().unwrap()), 16);
     }
 
     #[test]
     fn solve_part_2() {
-        assert_eq!(super::solve_part_2(TEST_INPUT), 0);
+        let input = include_str!("../inputs/prob21");
+        //assert_eq!(super::solve(input.parse::<Grid<101>>().unwrap()), 9279);
+        //assert_eq!(super::solve(input.parse::<Grid<201>>().unwrap()), 36525);
+        //assert_eq!(super::solve(input.parse::<Grid<251>>().unwrap()), 56624);
+        //assert_eq!(super::solve(input.parse::<Grid<501>>().unwrap()), 224670);
+
+        assert_eq!(super::solve(input.parse::<Grid<1001>>().unwrap()), 895133);
+        assert_eq!(super::solve(input.parse::<Grid<2001>>().unwrap()), 3569651);
+        assert_eq!(super::solve(input.parse::<Grid<5001>>().unwrap()), 22283554);
+        assert_eq!(super::solve(input.parse::<Grid<5005>>().unwrap()), 22318766);
     }
 
-    const TEST_INPUT: &'static str = "";
+    #[test]
+    fn real_test_part_2() {
+        let input = include_str!("../inputs/prob21");
+        assert_eq!(
+            super::solve(input.parse::<Grid<26501365>>().unwrap()),
+            625587097150084
+        );
+    }
+
+    const TEST_INPUT: &'static str = "...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........";
 }
